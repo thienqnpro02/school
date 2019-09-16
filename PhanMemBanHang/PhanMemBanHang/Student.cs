b8342c336ae4b48cf5e14748dca4475fe28941fb
Thanh Thien <thienqnpro02@gmail.com>
2019-09-16 11:18:26 +0700

    testing some stuff

---
+++ PhanMemBanHang/PhanMemBanHang/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhanMemBanHang
+{
+    public class Student
+    {
+        public string name { get; set; }
+        public string path { get; set; }
+        public Image picture
+        {
+            get
+            {
+                if(!String.IsNullOrEmpty(this.path))
+                {
+                    if (File.Exists(this.path))
+                    {
+                        return resizeImg(Image.FromFile(this.path), new Size(80, 80));
+                    }
+                    
+                }
+                return null;
+            }
+        }
+
+        private Image resizeImg(Image img, Size size)
+        {
+            return (Image)(new Bitmap(img, size));
+        }
+
+        public Student()
+        {
+
+        }
+    }
+}
