e82c1f0afa3692e3916a98599caa34759447b30a
Thanh Thien <thienqnpro02@gmail.com>
2019-10-17 10:55:24 +0700

    build class DanhMuc

---
+++ BanLinhKien/DTO/DanhMuc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DanhMuc
+    {
+        private int maDM;
+        private string tenDanhMuc;
+        private DateTime ngayTao;
+
+
+        public int MaDM { get => maDM; set => maDM = value; }
+        public string TenDanhMuc { get => tenDanhMuc; set => tenDanhMuc = value; }
+        public DateTime NgayTao { get => ngayTao; set => ngayTao = value; }
+
+        public DanhMuc() { }
+
+        public DanhMuc(int madm, string tendanhmuc, DateTime ngaytao)
+        {
+            this.MaDM = madm;
+            this.TenDanhMuc = tendanhmuc;
+            this.NgayTao = ngaytao;
+        }
+    }
+}
