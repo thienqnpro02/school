f94a2c18aeb092c6479ffcaf662db5eabb5a1d50
Thanh Thien <thienqnpro02@gmail.com>
2019-10-17 11:03:15 +0700

    build class PhieuNhap & ChiTietPhieuNhap

---
+++ BanLinhKien/DTO/ChiTietPhieuNhap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ChiTietPhieuNhap
+    {
+        private int maPhieuNhap;
+        private int maHang;
+        private int soLuong;
+        private int giaNhap;
+
+
+        public int MaPhieuNhap { get => maPhieuNhap; set => maPhieuNhap = value; }
+        public int MaHang { get => maHang; set => maHang = value; }
+        public int SoLuong { get => soLuong; set => soLuong = value; }
+        public int GiaNhap { get => giaNhap; set => giaNhap = value; }
+
+        public ChiTietPhieuNhap() { }
+
+        public ChiTietPhieuNhap(int maphieunhap, int mahang, int soluong, int gianhap)
+        {
+            this.MaPhieuNhap = maphieunhap;
+            this.MaHang = mahang;
+            this.SoLuong = soluong;
+            this.GiaNhap = gianhap;
+        }
+    }
+}
