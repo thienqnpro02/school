63e0af06684418ca0f84838ef355f88dd4d6bce3
HoaiNam <hoaimongktk20@gmail.com>
2019-10-17 13:33:20 +0700

    Nguyen mau

--- BanLinhKien/DTO/DanhMuc.cs
+++ BanLinhKien/DTO/DanhMuc.cs
@@ -8,22 +8,6 @@ namespace DTO
 {
     public class DanhMuc
     {
-        private int maDM;
-        private string tenDanhMuc;
-        private DateTime ngayTao;
 
-
-        public int MaDM { get => maDM; set => maDM = value; }
-        public string TenDanhMuc { get => tenDanhMuc; set => tenDanhMuc = value; }
-        public DateTime NgayTao { get => ngayTao; set => ngayTao = value; }
-
-        public DanhMuc() { }
-
-        public DanhMuc(int madm, string tendanhmuc, DateTime ngaytao)
-        {
-            this.MaDM = madm;
-            this.TenDanhMuc = tendanhmuc;
-            this.NgayTao = ngaytao;
-        }
     }
 }
