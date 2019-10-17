a42d5d3f4b8487b88d6639dcbbaf9c90c029e980
Thanh Thien <thienqnpro02@gmail.com>
2019-10-17 10:47:14 +0700

    build class KhachHang

---
+++ BanLinhKien/DTO/KhachHang.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class KhachHang
+    {
+        private int maKH;
+        private string hoTen;
+        private string sdt;
+        private DateTime namSinh;
+        private DateTime ngayTao;
+
+
+        public int MaKH { get => maKH; set => maKH = value; }
+        public string HoTen { get => hoTen; set => hoTen = value; }
+        public string Sdt { get => sdt; set => sdt = value; }
+        public DateTime NamSinh { get => namSinh; set => namSinh = value; }
+        public DateTime NgayTao { get => ngayTao; set => ngayTao = value; }
+
+        public KhachHang() { }
+
+        public KhachHang(int makh, string hoten, string sdt, DateTime namsinh, DateTime ngaytao)
+        {
+            this.MaKH = makh;
+            this.HoTen = hoten;
+            this.NamSinh = namsinh;
+            this.NgayTao = ngaytao;
+        }
+    }
+}
